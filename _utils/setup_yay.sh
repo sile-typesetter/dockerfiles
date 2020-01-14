2b02ef7dd7f6fd50f3c8e2da5ff4baf84cffa23f
Caleb Maclennan <caleb@alerque.com>
2020-01-14 18:17:50 +0300

    chore(images): Setup yay AUR helper to build sile deps

---
+++ _utils/setup_yay.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+
+pacman --needed --noconfirm --asdeps -S go
+
+useradd -m docker
+echo "docker:" | chpasswd -e
+mkdir -p /etc/sudoers.d
+echo "docker ALL = NOPASSWD: ALL" > /etc/sudoers.d/99-docker
+
+su docker -c 'cd; git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -i --noconfirm --asdeps'
+
+# rm -rf /home/docker/yay
+# pacman -Rns go
